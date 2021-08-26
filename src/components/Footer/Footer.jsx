c104d17e74c9a44e4198836aa0dc8e5ac5f4107c
Prajesh Yeotikar <yeotikarprajesh@gmail.com>
2021-08-26 21:14:47 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,69 @@
+import s from './Footer.module.scss';
+import { AiFillGithub } from 'react-icons/ai';
+import {
+  FaFacebookSquare,
+  FaLinkedinIn,
+  FaTelegramPlane,
+} from 'react-icons/fa';
+
+const Footer = () => {
+  let date = new Date();
+  let year = date.getFullYear();
+
+  return (
+    <div className={s.footer}>
+      <div className={s.container}>
+        <div className={s.copyright}>
+          <h3>Designed and Developed by Snaichuk Volodymyr</h3>
+        </div>
+
+        <div className={s.copyright}>
+          <h3>Copyright © {year} SV.dev</h3>
+        </div>
+
+        <div className={s.body}>
+          <ul className={s.socialIcons}>
+            <li>
+              <a
+                href="https://github.com/prake"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebookSquare />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://t.me/prajeshy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTelegramPlane />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/prajesh-yeotikar-a7830a1b8/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/scenes/Home/IntroSection/IntroSection.jsx
@@ -0,0 +1,114 @@
+import s from './IntroSection.module.scss';
+import avatar from '../../../assets/avatar.svg';
+import { AiFillGithub } from 'react-icons/ai';
+import {
+  FaFacebookSquare,
+  FaLinkedinIn,
+  FaTelegramPlane,
+} from 'react-icons/fa';
+import Tilt from 'react-parallax-tilt';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+const IntroSection = () => {
+  return (
+    <section className={s.content}>
+      <div className={s.introduction}>
+        <div className={s.introductionText}>
+          <h1 className={s.title}>
+            LET ME <span className={s.purple}> INTRODUCE </span>{' '}
+            MYSELF
+          </h1>
+
+          <div className={s.description}>
+            <p>
+              I fell in love with programming and I have at least
+              learnt something, I think… 🤷‍♂️
+            </p>
+
+            <p>
+              My field of Interest's are 
+              <i>
+                <b className={s.purple}>
+                  {' '}
+                  Web-App Security,Network Security,Information Security,Ethical Hacking,Vulnerability Testing,etc.
+                </b>
+              </i>
+            </p>
+
+            <p>
+              A passionate and self-motivated cybersecurity enthusiast 🚀 <br />I
+              
+              <i>
+                <b className={s.purple}>Web and Mobile</b>
+              </i>{' '}
+              applications with
+              <br />
+              <i>
+                <b className={s.purple}>
+                  JavaScript, Reactjs, Nodejs, React Native
+                </b>
+              </i>{' '}
+              and some other cool libraries
+            </p>
+          </div>
+        </div>
+
+        <Tilt trackOnWindow={true}>
+          <LazyLoadImage alt="avatar" effect="blur" src={avatar} />
+        </Tilt>
+      </div>
+
+      <div className={s.introSocial}>
+        <h1>FIND ME ON</h1>
+        <p>
+          Feel free to <span className={s.purple}>connect </span>with
+          me
+        </p>
+        <ul className={s.socialLinks}>
+          <li className={s.socialLink}>
+            <a
+              href="https://github.com/prajeshy"
+              target="_blank"
+              rel="noreferrer"
+              className={s.socialIcon}
+            >
+              <AiFillGithub />
+            </a>
+          </li>
+          <li className={s.socialLink}>
+            <a
+              href="https://www.facebook.com/Snaychuk"
+              target="_blank"
+              rel="noreferrer"
+              className={s.socialIcon}
+            >
+              <FaFacebookSquare />
+            </a>
+          </li>
+          <li className={s.socialLink}>
+            <a
+              href="https://t.me/prajeshy"
+              target="_blank"
+              rel="noreferrer"
+              className={s.socialIcon}
+            >
+              <FaTelegramPlane />
+            </a>
+          </li>
+          <li className={s.socialLink}>
+            <a
+              href="https://www.linkedin.com/in/prajesh-yeotikar-a7830a1b8/"
+              target="_blank"
+              rel="noreferrer"
+              className={s.socialIcon}
+            >
+              <FaLinkedinIn />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default IntroSection;
